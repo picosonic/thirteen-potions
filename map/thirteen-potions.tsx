8a3061d7377c9be58f17dcf9aaf133288b6afe66
Jasper Renow-Clarke <jrenow_clarke@yahoo.co.uk>
2023-09-15 23:28:16 +0100

    Adjust num tiles due to knight

--- map/thirteen-potions.tsx
+++ map/thirteen-potions.tsx
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.2" name="thirteen-potions" tilewidth="16" tileheight="16" tilecount="132" columns="12">
+<tileset version="1.10" tiledversion="1.10.2" name="thirteen-potions" tilewidth="16" tileheight="16" tilecount="144" columns="12">
  <editorsettings>
   <export target="" format="tsx"/>
  </editorsettings>
